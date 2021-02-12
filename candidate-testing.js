64e4bab91e0ebd58a4d57b152da8bccf1d042161
naisiay <naisiayee@gmail.com>
2021-02-12 07:25:30 +0000

    new declarations

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,8 +8,8 @@ let candidateName = '';
 let question = '';
 let correctAnswer = '';
 let candidateAnswer = '';
-let questions = ["Who was the first American woman in space? ","True or false: 5000 meters = 5 kilometers ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the International Space Station (ISS)? "];
-let correctAnswers = ["Sally Ride","True","40","Trajectory","3"];
+let questions = ["True or false: 5000 meters = 5 kilometers ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","Who was the first American woman in space?  ","What is the minimum crew size for the International Space Station (ISS)? "];
+let correctAnswers = ["True","40","Trajectory","Sally Ride","3"];
 let candidateAnswers = [];
 
 
@@ -54,7 +54,7 @@ if (grade >= 80) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  //console.log(`Hello ${candidateName}`);
+  //console.log(`hello: ${candidateName}`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
