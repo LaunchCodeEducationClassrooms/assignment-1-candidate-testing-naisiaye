04a4374d832cf2610ba7a7c10646afcebb9009ea
naisiay <naisiayee@gmail.com>
2021-02-12 08:36:41 +0000

    Modified

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,7 +11,6 @@ let candidateAnswer = '';
 let questions = ["True or false: 5000 meters = 5 kilometers ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","Who was the first American woman in space?  ","What is the minimum crew size for the International Space Station (ISS)? "];
 let correctAnswers = ["True","40","Trajectory","Sally Ride","3"];
 let candidateAnswers = [];
-let counter = 0;
 let status = '';
 
 function askForName() {
@@ -36,18 +35,18 @@ function gradeQuiz(candidateAnswers) {
   correctAnswers = correctAnswers.join(',').toLowerCase().split(',');
   for (let j=0; j<candidateAnswers.length; j++){
     if (candidateAnswers[j] === correctAnswers[j]){
-      counter+=1;
+      grade+=1;
     } else {
-      counter +=0;
+      grade+=0;
     }
   } 
-  grade = counter / (questions.length) *100;
+  grade = grade / (questions.length) *100;
   if (grade >= 80) {
     status = 'PASSED';
   } else {
     status = 'FAILED';
   }
-console.log(`>>> Overall Grade: ${grade}% (${counter} of ${correctAnswers.length} responses correct)\n>>> Status: ${status} >>>`)
+console.log(`>>> Overall Grade: ${grade}% (${grade/100*5} of ${correctAnswers.length} responses correct)\n>>> Status: ${status} >>>`)
 
   return grade;
 }
