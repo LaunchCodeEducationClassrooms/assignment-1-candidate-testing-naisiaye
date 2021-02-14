9716098735926a794b01d3bb28b9042f82a474b9
naisiay <naisiayee@gmail.com>
2021-02-14 19:02:23 +0000

    modified

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,15 +1,11 @@
 const input = require('readline-sync');
 
-// TODO 2: modify your quiz app to ask 5 questions //
-
-// TODO 1.1a: Define candidateName // 
-let candidateName = '';
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = 'Who was the first American woman in space? ';
-let correctAnswer = 'Sally Ride';
-let candidateAnswer = '';
-let questions = ['True or false: 5 kilometer == 5000 meters? ',"(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","Who was the first American woman in space? ","What is the minimum crew size for the ISS? "];
-let correctAnswers = ["true","40","Trajectory","Sally Ride","3"];
+let candidateName = "";
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
 function askForName() {
@@ -44,6 +40,7 @@ function gradeQuiz(candidateAnswers) {
   }
   return grade;
 }
+
 
 function runProgram() {
   askForName();
