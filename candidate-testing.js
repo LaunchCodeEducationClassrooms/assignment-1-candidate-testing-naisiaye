d5a8f175b79b6bf2190c24c7cb0c75b76f5fe127
naisiay <naisiayee@gmail.com>
2021-02-14 18:27:38 +0000

    modified

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,7 @@ let candidateName = '';
 let question = 'Who was the first American woman in space? ';
 let correctAnswer = 'Sally Ride';
 let candidateAnswer = '';
-let questions = ["True or false: 5000 meters = 5 kilometers ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","Who was the first American woman in space?  ","What is the minimum crew size for the International Space Station (ISS)? "];
+let questions = ['True or false: 5 kilometer == 5000 meters? ',"(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","Who was the first American woman in space? ","What is the minimum crew size for the ISS? "];
 let correctAnswers = ["True","40","Trajectory","Sally Ride","3"];
 let candidateAnswers = [];
 
